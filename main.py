d59619c1b7f599a68193463841c3ad7332ef9fee
toithichcakhia <toidicakhia1234@gmail.com>
2022-08-12 09:31:37 +0700

    Update code

---
+++ main.py
@@ -0,0 +1,20 @@
+import argparse
+
+
+
+
+def parse_argument():
+	'''
+		Function to parse argument from CLI
+		Ex: python main.py --install fdpclient --version 4.4.5
+	'''
+
+	pass
+
+
+def main():
+	parse_argument()
+
+
+if __name__ == '__main__':
+	main()
